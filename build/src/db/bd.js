8a628b0002f9388169fbc7986d28c57c63cb8c02
enison <enisonoliveira@hotmail.com>
2022-03-10 23:55:27 -0300

    clear project

--- build/src/db/bd.js
+++ build/src/db/bd.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.connect('mongodb://localhost:27017/web', {
+mongoose_1.default.connect('mongodb://mongo:27017/web', {
     useNewUrlParser: true,
     useFindAndModify: true,
     useUnifiedTopology: true
